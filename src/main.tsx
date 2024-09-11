ee3ab5e405b9a5e397cc465398332343e4671c1e
zaymovskey <zaymovskey@mail.com>
2024-09-11 19:34:16 +0300

    normalize scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-import App from './app/App.tsx';
+import App from '@/app/App';
+import '@/app/styles/normalize.scss';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
