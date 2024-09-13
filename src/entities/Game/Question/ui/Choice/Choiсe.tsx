e69332e595ed6241ab4c6b9906b52e4e5213e381
zaymovskey <zaymovskey@mail.com>
2024-09-13 16:19:01 +0300

    переключение выбранного ответа

--- src/entities/Game/Question/ui/Choice/Choiсe.tsx
+++ src/entities/Game/Question/ui/Choice/Choiсe.tsx
@@ -1,9 +1,11 @@
 import cls from './Choice.module.scss';
+import { IWord } from '@/DB.tsx';
 import { useAppSelector } from '@/app/providers/StoreProvider/lib/hooks.ts';
+import { classNames } from '@/shared/lib/utils/classNames/classNames.ts';
 import { getRandomNumberFromInterval } from '@/shared/lib/utils/getRandomNumberFromInterval/getRandomNumberFromInterval.ts';
 import { getRandomUniqueElements } from '@/shared/lib/utils/getRandomUniqueElements/getRandomUniqueElements.ts';
 import { Typography } from 'antd';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
 export const Choice = () => {
   const wordsCount = useMemo(() => getRandomNumberFromInterval(2, 6), []);
@@ -17,11 +19,26 @@ export const Choice = () => {
     []
   );
 
+  const [selectedAnswer, setSelectedAnswer] = useState<IWord | undefined>(
+    undefined
+  );
+
+  const handleSelectAnswer = (word: IWord) => {
+    setSelectedAnswer(word);
+  };
+
   return (
     <div>
       <div className={cls.variants}>
         {currentQuestionWords.map((word) => (
-          <button className={cls.variantsItem}>
+          <button
+            onClick={() => handleSelectAnswer(word)}
+            className={classNames(
+              cls.variantsItem,
+              { [cls.selected]: selectedAnswer?.russian === word.russian },
+              []
+            )}
+          >
             <word.Icon color={'inherit'} width={40} height={40} />
             <Typography.Text>{word.russian}</Typography.Text>
           </button>
