7bc942751ca3cc50768ab1a4fcd18e893e31aa90
zaymovskey <zaymovskey@mail.com>
2024-11-02 20:46:09 +0200

    попытка исправить проблему с анимацией exit

--- src/app/providers/AppRouter/ui/AppRouter.tsx
+++ src/app/providers/AppRouter/ui/AppRouter.tsx
@@ -1,11 +1,12 @@
 import { RouteConfig } from '../lib/routeConfig';
 import { PageLoader } from '@/shared/ui';
 import { type FC, Suspense } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 
 const AppRouter: FC = () => {
+  const location = useLocation();
   return (
-    <Routes>
+    <Routes location={location} key={location.pathname}>
       <Route path="/" element={<Navigate to="/topics" replace />} />
       {Object.values(RouteConfig).map(({ path, element }) => (
         <Route
